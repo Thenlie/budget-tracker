2e613f5a0baf40a1f3c264ac36da77d99fa8be64
Leithen <leithen113@gmail.com>
2022-01-18 15:53:02 -0700

    First Heroku deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget-tracker";
 
 const app = express();
 
